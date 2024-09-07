106a99dbe58fdb0456cc45ffc80808829dccc1a8
Jeffrey Alex Clark <aclark@aclark.net>
2024-09-07 10:57:06 -0400

    Rename

--- add_shortcuts_folder_to_dock.sh
+++ add_shortcuts_folder_to_dock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the dock path (formerly folder path) you want to add to the Dock
-DOCK_PATH="$HOME/Dotfiles/dock"
+DOCK_PATH="$HOME/Dotfiles/server"
 
 # Check if the dock is already in the Dock
 if ! defaults read com.apple.dock persistent-others | grep -q "$DOCK_PATH"; then
