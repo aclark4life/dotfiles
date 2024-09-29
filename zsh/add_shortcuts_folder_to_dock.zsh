ce161695393414e765381271b7e12d1c1f95ecc2
Jeffrey A. Clark <aclark@aclark.net>
2024-09-29 14:43:26 -0400

    Darwin-only

--- zsh/add_shortcuts_folder_to_dock.zsh
+++ zsh/add_shortcuts_folder_to_dock.zsh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-# Define the dock path (formerly folder path) you want to add to the Dock
-DOCK_PATH="$HOME/Dotfiles/server"
+case `uname` in
+  Darwin)
+	# Define the dock path (formerly folder path) you want to add to the Dock
+	DOCK_PATH="$HOME/Dotfiles/server"
 
-# Check if the dock is already in the Dock
-if ! defaults read com.apple.dock persistent-others | grep -q "$DOCK_PATH"; then
-    # Add the dock (folder) to the Dock
-    defaults write com.apple.dock persistent-others -array-add \
-    "<dict>
-        <key>tile-data</key>
-        <dict>
-            <key>file-data</key>
-            <dict>
-                <key>_CFURLString</key>
-                <string>file://$DOCK_PATH</string>
-                <key>_CFURLStringType</key>
-                <integer>15</integer>
-            </dict>
-        </dict>
-        <key>tile-type</key>
-        <string>directory-tile</string>
-    </dict>"
+	# Check if the dock is already in the Dock
+	if ! defaults read com.apple.dock persistent-others | grep -q "$DOCK_PATH"; then
+	    # Add the dock (folder) to the Dock
+	    defaults write com.apple.dock persistent-others -array-add \
+	    "<dict>
+		<key>tile-data</key>
+		<dict>
+		    <key>file-data</key>
+		    <dict>
+			<key>_CFURLString</key>
+			<string>file://$DOCK_PATH</string>
+			<key>_CFURLStringType</key>
+			<integer>15</integer>
+		    </dict>
+		</dict>
+		<key>tile-type</key>
+		<string>directory-tile</string>
+	    </dict>"
 
-    # Restart the Dock to apply changes
-    killall Dock
-fi
+	    # Restart the Dock to apply changes
+	    killall Dock
+	fi
+	;;
+esac
