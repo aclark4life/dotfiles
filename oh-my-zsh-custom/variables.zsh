4f36a0b6203e71138d997bf394ccdf68929860e4
Jeffrey A. Clark <aclark@aclark.net>
2025-04-27 18:04:25 -0400

    Add tmuxai key

--- oh-my-zsh-custom/variables.zsh
+++ oh-my-zsh-custom/variables.zsh
@@ -11,6 +11,3 @@ export NVM_DIR="$HOME/.nvm"
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-
-
-export TMUXAI_OPENROUTER_API_KEY="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"
