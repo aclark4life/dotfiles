da4cc71eed6f042f8de45c13409bcedd31dd8c4b
Jeffrey A. Clark <aclark@aclark.net>
2025-05-15 21:06:17 -0400

    Update dotfiles

--- oh-my-zsh-custom/variables.zsh
+++ oh-my-zsh-custom/variables.zsh
@@ -5,9 +5,14 @@ export EDITOR="nvim"
 
 export HOMEBREW_NO_ENV_HINTS=1
 
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+
+case `uname` in
+  Darwin)
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+    [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+    ;;
+esac
