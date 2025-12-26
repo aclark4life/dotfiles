f1035b75e621c470032abbbc261b7a071ae4d8f9
Jeffrey A. Clark <aclark@aclark.net>
2025-12-26 13:09:34 -0500

    Update dotfiles

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,14 +1,3 @@
--- Set options for Python filetype                                        
-vim.api.nvim_create_autocmd("FileType", {                                 
-  pattern = "python",                                                     
-  callback = function()                                                   
-    vim.bo.tabstop = 4                                                    
-    vim.bo.shiftwidth = 4                                                 
-    vim.bo.expandtab = true                                               
-  end,                                                                    
-})                                                                        
-                                                                          
--- Set global options                                                     
 vim.o.mouse = ""                                                          
 vim.o.tabstop = 4                                                         
 vim.o.shiftwidth = 4                                                      
